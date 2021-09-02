bb1f929bd50b3ca7c50d16ae09d00d1da4174f4e
Pikasso <best98@free.fr>
2021-09-02 15:31:57 +0200

    Update BenchPi.py

--- BenchPi.py
+++ BenchPi.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
+import sys
 import subprocess
 import os
 import time
 from datetime import datetime
 
-# OPTIONS : number and size of pictures to be resized 
+# OPTIONS : number and size of pictures to be resized
 number = 4
-size = "m"   # s / m / l
+size   = "m"   # s / m / l
+verbose= 0
+multi  = " &"  # Either " &" or ""
+convertcmd = ""
+
+if len(sys.argv) > 1 :
+        verbose = int(sys.argv[1])
+        number  = int(sys.argv[2])
+        size    = sys.argv[3]
+
+
 
 def cleanup():
         for image in range(1,number+2):
@@ -16,21 +27,23 @@ def cleanup():
 
 cleanup()
 start = time.time()
-print(datetime.now().strftime("%H:%M:%S") + "   > Start benchmark")
+if len(sys.argv)>1:print( open('/sys/firmware/devicetree/base/model', 'r').read() + "  > " + str(number) + " x " + size)
+
+if verbose:print(datetime.now().strftime("%H:%M:%S") + "   > Start benchmark")
 for image in range(1,number+1):
         convertarg = size + "img.jpg -mattecolor white -frame 30 -background transparent -rotate "+ str(image*45) +" -resize 73% img"+str(image)+".png"
-        #subprocess.run (['convert ' +convertarg+ ' && echo 1 > '+ str(image)+ '.txt'], shell=True)  #1 /+/ sequential
-        subprocess.run (['convert ' +convertarg+ ' && echo 1 > '+ str(image)+ '.txt &'], shell=True)  #1 // parallel
-        print(datetime.now().strftime("%H:%M:%S") + "   >> Start Thread"+ str(image)+ ": convert "+size + "img.jpg -mattec ... img"+str(image)+".png" )
+        subprocess.run (['convert ' +convertarg+ ' && echo 1 > '+ str(image)+ '.txt' + multi], shell=True)  #1 // parallel
+        convertcmd += " -composite img" + str(image) + ".png -geometry +930+100"
+        if verbose:print(datetime.now().strftime("%H:%M:%S") + "   >> Start Thread"+ str(image)+ ": convert "+size + "img.jpg -mattec ... img"+str(image)+".png" )
 for image in range(1,number+1):
         while not (os.path.exists(str(image)+".txt")):
                 time.sleep(1)
-print(datetime.now().strftime("%H:%M:%S") + "   << End Threads")
-convertcmd = "logo.png -mattecolor white -frame 30 img1.png -geometry +150+80 -composite img2.png -geometry +930+100 -composite img3.png -geometry +550+560 -composite img4.png -geometry +1190+580 -composite finale.jpg"
+if verbose:print(datetime.now().strftime("%H:%M:%S") + "   << End Threads")
+convertcmd = "logo.png -mattecolor white -frame 30 img1.png -geometry +150+80" + convertcmd + " -composite finale.jpg"
 subprocess.run (['convert ' +convertcmd+ ' && echo 5 > 5.txt'], shell=True)  #2
 while not (os.path.exists("5.txt")):
         time.sleep(1)
-print(datetime.now().strftime("%H:%M:%S") + "   < End benchmark ")
-print( "  > "+ str(round(time.time()- start, 3)) + " Secondes")
+if verbose:print(datetime.now().strftime("%H:%M:%S") + "   < End benchmark ")
+print( open('/sys/firmware/devicetree/base/model', 'r').read() + "  > " + str(number) + " x " + size + " = "+ str(round(time.time()- start, 3)) + " sec")
 cleanup()
 quit()
